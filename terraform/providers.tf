8c6d5283f00347a86df22a9d24590ddcec7b3183
cameron <cmonthe8@outlook.com>
2025-09-25 13:00:29 -0400

    adding tf folder

---
+++ terraform/providers.tf
@@ -0,0 +1,22 @@
+
+terraform {
+  required_version = ">= 1.8.0"
+
+  backend "s3" {
+    bucket         = "tfstate-nqdexu"
+    region         = "us-east-1"
+    dynamodb_table = "tf-locks"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
