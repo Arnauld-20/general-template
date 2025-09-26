7c0c5bb075e0c01012afeefdf6cc248717659a3c
cameron <cmonthe8@outlook.com>
2025-09-26 14:53:25 -0400

    update provider bucket key

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,7 +3,8 @@ terraform {
   required_version = ">= 1.8.0"
 
   backend "s3" {
-    bucket         = "tfstate-nqdexu"
+    bucket         = "tfstate-cif0ip"
+    key            = "cmonthe2/repo/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "tf-locks"
     encrypt        = true
